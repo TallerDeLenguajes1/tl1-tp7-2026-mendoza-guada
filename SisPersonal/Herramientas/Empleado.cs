5f8cf4716533d510b53478ac13de1142247ed87a
Guadalupe Mendoza <cuki13lucky@gmail.com>
2026-05-26 16:46:06 -0300

    Ej 2

---
+++ SisPersonal/Herramientas/Empleado.cs
@@ -0,0 +1,13 @@
+namespace EspacioEmpleado
+{
+    class Empleado
+    {
+        public string Nombre;
+        public string Apellido;
+        public  DateTime FechaNac;
+        public  char EstadoCivil;
+        public DateTime FechaIngreso;
+
+        private double sueldoBasico;
+    }
+}
