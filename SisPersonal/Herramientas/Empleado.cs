d28787a5559e4b64c4d1fbdbfc8c1086c3eb9107
Guadalupe Mendoza <cuki13lucky@gmail.com>
2026-07-06 02:04:29 -0300

    Clase empleado y métodos completo, prueba con empleado 1

--- SisPersonal/Herramientas/Empleado.cs
+++ SisPersonal/Herramientas/Empleado.cs
@@ -1,13 +1,67 @@
-namespace EspacioEmpleado
+class Empleado
 {
-    class Empleado
-    {
-        public string Nombre;
-        public string Apellido;
-        public  DateTime FechaNac;
-        public  char EstadoCivil;
-        public DateTime FechaIngreso;
+    public string? Nombre {get; set; }
+    public string? Apellido {get; set; }
+    public  DateTime FechaNac {get; set; }
+    public  char EstadoCivil {get; set; }
+    public DateTime FechaIngreso {get; set; }
+    public double SueldoBasico {get; set; }
+    public Cargos Cargo {get; set; }
 
-        private double sueldoBasico;
+    //CONSTRUCTOR//
+    public Empleado(string nombre, string apellido, DateTime fechanac, char estadocivil, DateTime fechaingreso, double sueldobasico, Cargos cargo)
+    {
+        Nombre=nombre;
+        Apellido=apellido;
+        FechaNac=fechanac;
+        EstadoCivil=estadocivil;
+        FechaIngreso=fechaingreso;
+        SueldoBasico=sueldobasico;
+        Cargo=cargo;
     }
+    public int Antiguedad() 
+    {
+        DateTime hoy = DateTime.Today;
+        return (hoy.Year - FechaIngreso.Year);
+    }
+    public int EdadEmpleado()
+    {
+        DateTime hoy = DateTime.Today;
+        return (hoy.Year - FechaNac.Year);//REVISAR POR SI EL EMPLEADO CUMPLE DESPUES DEL MES QUE ES HOY
+    }
+    public int AniosParaJubilarse()
+    {
+        return 65-EdadEmpleado();
+    }
+    public double Salario()
+    {   
+        double Adicional;
+        if (Antiguedad()<=20)
+        {
+            Adicional=Antiguedad()*1/100*SueldoBasico;
+        } else
+        {
+            Adicional=25/100*SueldoBasico;
+        }
+        if (Cargo==Cargos.Especialista || Cargo==Cargos.Ingeniero)
+        {
+            Adicional+=Adicional*50/100;
+        }
+        if (EstadoCivil=='C')
+        {
+            Adicional+=150000;
+        }
+        double Salario = SueldoBasico + Adicional;
+        return Salario;
+    }
+
+}
+
+enum Cargos
+{
+    Auxiliar=0,
+    Administrativo=1,
+    Ingeniero=2,
+    Especialista=3,
+    Investigador=4
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Empleado empleado1= new Empleado("Martín", "González", new DateTime(1999,12,5), 'C', new DateTime(2015,08,21), 800000, Cargos.Ingeniero);
+
+Console.WriteLine("====EMPLEADO #1====\nNombre: "+empleado1.Nombre+" "+empleado1.Apellido+"\nFecha de nacimiento: "+empleado1.FechaNac.ToString("dd/MM/yyyy")+"\nEdad: "+empleado1.EdadEmpleado()+"\nFecha de ingreso: "+empleado1.FechaIngreso.ToString("dd/MM/yyyy")+"\nSueldo básico: $"+empleado1.SueldoBasico+"\nCargo: "+empleado1.Cargo+"\nAntigüedad: "+empleado1.Antiguedad()+"\nTotal a cobrar: $"+empleado1.Salario());
+
+
